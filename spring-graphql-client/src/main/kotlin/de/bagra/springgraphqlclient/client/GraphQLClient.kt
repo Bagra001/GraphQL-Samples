e07e921fb80ff9ca399683659d99dbfe5d99bfe3
bgrabelus <bartlomiej.grabelus@interhyp.de>
2023-02-14 13:35:27 +0100

    refactoring

--- spring-graphql-client/src/main/kotlin/de/bagra/springgraphqlclient/client/GraphQLClient.kt
+++ spring-graphql-client/src/main/kotlin/de/bagra/springgraphqlclient/client/GraphQLClient.kt
@@ -14,60 +14,56 @@ class GraphQLClient(@Value("\${graphql.server.baseurl}") final var qraphQlServer
     private val httpGraphQlClient = HttpGraphQlClient.builder(webClient)
         .build()
 
-    fun getAllCars(): List<CarDto> {
-        return httpGraphQlClient
-            .mutate()
-            .build()
-            .documentName("cars-query")
-            .retrieve("cars")
-            .toEntityList(CarDto::class.java)
-            .share()
-            .block()?.stream()?.toList() ?: emptyList()
+    fun getAllCars(): List<CarDto?> {
+        return queryList("cars-query", "cars")
     }
 
-    fun getBMWCars(): List<CarDto> {
-        return httpGraphQlClient
-            .mutate()
-            .build()
-            .documentName("cars-by-brand")
-            .variable("marke", "BMW")
-            .retrieve("carsByBrand")
-            .toEntityList(CarDto::class.java)
-            .share()
-            .block()?.stream()?.toList() ?: emptyList()
+    fun getBMWCars(): List<CarDto?> {
+        return queryList("cars-by-brand", "carsByBrand","marke", "BMW" )
     }
 
-    fun getMercedesCars(): List<CarDto> {
-        return httpGraphQlClient
-            .mutate()
-            .build()
-            .documentName("cars-by-brand")
-            .variable("marke", "Mercedes")
-            .retrieve("carsByBrand")
-            .toEntityList(CarDto::class.java)
-            .share()
-            .block()?.stream()?.toList() ?: emptyList()
+    fun getMercedesCars(): List<CarDto?> {
+        return queryList("cars-by-brand", "carsByBrand","marke", "Mercedes" )
     }
 
     fun getCarByFIN(fin: String): CarDto? {
+        return query("cars-by-fin", "carsByFin","fahrzeugidnummer", fin )
+    }
+
+    fun getCarByModel(model: String): CarDto? {
+        return query("cars-by-model", "carsByModel","fahrzeugmodel", model )
+    }
+    
+    private fun query(documentName: String, retrieveName: String, variableKey: String, variableValue: String): CarDto? {
         return httpGraphQlClient
             .mutate()
             .build()
-            .documentName("cars-by-fin")
-            .variable("fahrzeugidnummer", fin)
-            .retrieve("carsByFin")
+            .documentName(documentName)
+            .variable(variableKey, variableValue)
+            .retrieve(retrieveName)
             .toEntity(CarDto::class.java)
             .share()
             .block()
     }
 
-    fun getCarByModel(model: String): List<CarDto> {
+    private fun queryList(documentName: String, retrieveName: String, variableKey: String, variableValue: String): List<CarDto?> {
         return httpGraphQlClient
             .mutate()
             .build()
-            .documentName("cars-by-model")
-            .variable("fahrzeugmodel", model)
-            .retrieve("carsByModel")
+            .documentName(documentName)
+            .variable(variableKey, variableValue)
+            .retrieve(retrieveName)
+            .toEntityList(CarDto::class.java)
+            .share()
+            .block()?.stream()?.toList() ?: emptyList()
+    }
+
+    private fun queryList(documentName: String, retrieveName: String): List<CarDto?> {
+        return httpGraphQlClient
+            .mutate()
+            .build()
+            .documentName(documentName)
+            .retrieve(retrieveName)
             .toEntityList(CarDto::class.java)
             .share()
             .block()?.stream()?.toList() ?: emptyList()
